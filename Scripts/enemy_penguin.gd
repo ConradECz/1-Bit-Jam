7b6907cfc3a558f5b49590473b635fc4dc17da8e
ConradECz <conradec21@gmail.com>
2025-11-19 22:41:03 -0500

    Added collision so that Enemy Penguin does not fall into hole

--- Scripts/enemy_penguin.gd
+++ Scripts/enemy_penguin.gd
@@ -12,7 +12,8 @@ var direction = 1
 func _process(_delta):
 	if ray_cast_right.is_colliding():
 		direction = -1
-	if ray_cast_left.is_colliding():
+	elif ray_cast_left.is_colliding():
 		direction = 1
-
+	if not ray_cast_down.is_colliding():
+		direction *= -1
 	position.x += direction * SPEED * _delta
