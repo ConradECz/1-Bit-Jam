029c2226c7bd04aad32271e856cbaae61523b101
ConradECz <conradec21@gmail.com>
2025-11-20 16:21:38 -0500

    Added animation in for Player Running + Jump

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -5,6 +5,8 @@ const JUMP_VELOCITY = -400
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -12,8 +14,25 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		AudioPlayer.play_sound("res://audio/sfx/jared_jump1.wav")
-		
+
+	# Direction: -1, 0, 1	
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	# Flip the Player
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	# Animations for Player
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
