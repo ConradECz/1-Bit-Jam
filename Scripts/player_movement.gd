6a16e62278c1278c901341e22b8a9bf84a200618
ConradECz <conradec21@gmail.com>
2025-11-22 23:30:24 -0500

    Height of jump depends on how long you press the jump button

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -400
+const jump_gravity_multipler = 3.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -97,7 +98,11 @@ func _physics_process(delta):
 			animated_sprite.animation_finished.connect(play_attack_sound, CONNECT_ONE_SHOT)
 	
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if !Input.is_action_pressed("jump") and velocity.y < 0:
+			velocity.y += gravity * delta * jump_gravity_multipler
+		else:
+			velocity.y += gravity * delta
+		
 		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
